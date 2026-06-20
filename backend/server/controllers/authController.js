7352565ae4cea55bac1e652826802c03f31fd0fe
BhanuJs-alt <knightmoon101@gmail.com>
2026-06-21 01:40:44 +0530

    implement login route and generate jwt

--- backend/server/controllers/authController.js
+++ backend/server/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req,res) => {
       try {
@@ -33,7 +34,6 @@ export const signup = async (req,res) => {
     
             res.status(201).json({
                 message:"user created",
-                user,
             });
         }
 
@@ -42,4 +42,48 @@ export const signup = async (req,res) => {
             message:error.message,
         })
       }
+}
+
+export const login = async(req,res) => {
+        try {  
+           const { email,password } = req.body;
+
+            if(!email || !password){
+                res.status(400).json({
+                    message:"email and password are required"
+                });
+            }
+           const user = await User.findOne({ email });
+
+           const isMatch = await bcrypt.compare(
+            password,
+            user.password
+           );
+
+           if(!user || !isMatch){
+             return res.status(400).json({
+                message:"Invalid ceredentials"
+            });
+           }
+
+            const token = jwt.sign(
+                {
+                 userId : user._id,
+                },
+                process.env.JWT_SECRET,
+                {
+                 expiresIn:"7d"
+                }
+            );
+ 
+             res.status(200).json({
+                 message: "Login successful",
+                 token,
+             });
+          
+        } catch (error) {
+            res.status(500).json({
+                message:error.message,
+            });
+        }
 }
